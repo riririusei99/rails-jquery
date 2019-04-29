8e8d65cad38604fe37fd3b92a405174c225a35f1
riririusei99 <riry1313@gmail.com>
2019-04-29 11:14:08 +0000

    study addEventListener

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+function printHelloWithButton() {
+  var btn = document.querySelector("input");
+
+  function printHello() {
+    console.log("Hello World dayo.");
+  }
+  btn.addEventListener("click", printHello);
+}
+
+window.addEventListener("load", printHelloWithButton);
+
+
+
+
