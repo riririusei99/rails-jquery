fcdab63d6ff2406b1b2c8a86d3df56ece4ef4be3
riririusei99 <riry1313@gmail.com>
2019-04-29 11:58:08 +0000

    study jQuery

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,8 +1,5 @@
 window.addEventListener("load", function() {
-  var btn = document.querySelector('input');
-  
-  btn.addEventListener("click", function() {
-    var p = document.querySelector("p");
-    p.innerText = "クリックされた！";
+  $("li").on("click", function() {
+    console.log(this.id);
   });
 });
