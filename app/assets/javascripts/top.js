cd742af3763cac6d59cc884a86fbf52576cf42c0
riririusei99 <riry1313@gmail.com>
2019-04-30 02:35:43 +0000

    study jQuery

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", function() {
   $("li").on("click", function() {
-    console.log(this.id);
+    $("li.selected").removeClass("selected");
+    $(this).addClass("selected");
+    $(".contents div").hide(); // 二つの要素を非表示にする
+    $("." + this.id).show(); // クリックされたボタンに対応する要素を表示する
   });
 });
